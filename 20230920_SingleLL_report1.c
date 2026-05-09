e32281d147a6dfe44c3a59b73f0ced901bab39b1
seahyun023 <20230920@vision.hoseo.edu>
2026-05-09 15:23:49 +0900

    자료구조.c

---
+++ 20230920_SingleLL_report1.c
@@ -0,0 +1,12 @@
+﻿'자료구조 과제3.c.exe'(Win32): 'C:\Users\young\source\repos\자료구조포인터.c\x64\Debug\자료구조 과제3.c.exe'을(를) 로드했습니다. 기호가 로드되었습니다.
+'자료구조 과제3.c.exe'(Win32): 'C:\Windows\System32\ntdll.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'자료구조 과제3.c.exe'(Win32): 'C:\Windows\System32\kernel32.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'자료구조 과제3.c.exe'(Win32): 'C:\Windows\System32\KernelBase.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'자료구조 과제3.c.exe'(Win32): 'C:\Windows\System32\vcruntime140d.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'자료구조 과제3.c.exe'(Win32): 'C:\Windows\System32\ucrtbased.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+25792 스레드가 종료되었습니다(코드: 0 (0x0)).
+'자료구조 과제3.c.exe'(Win32): 'C:\Windows\System32\kernel.appcore.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'자료구조 과제3.c.exe'(Win32): 'C:\Windows\System32\msvcrt.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+8920 스레드가 종료되었습니다(코드: 0 (0x0)).
+5892 스레드가 종료되었습니다(코드: 0 (0x0)).
+'[12216] 자료구조 과제3.c.exe' 프로그램이 종료되었습니다(코드: 0 (0x0)).
