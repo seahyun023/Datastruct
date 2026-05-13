e9870e9aa1166cd63850df698ac91ae1eac46a35
seahyun023 <20230920@vision.hoseo.edu>
2026-05-13 19:51:24 +0900

    자료구조.c

---
+++ 20230920_SingleLL_report2.c
@@ -0,0 +1,12 @@
+﻿'Project2.exe'(Win32): 'C:\Users\young\source\repos\자료구조포인터.c\x64\Debug\Project2.exe'을(를) 로드했습니다. 기호가 로드되었습니다.
+'Project2.exe'(Win32): 'C:\Windows\System32\ntdll.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'Project2.exe'(Win32): 'C:\Windows\System32\kernel32.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'Project2.exe'(Win32): 'C:\Windows\System32\KernelBase.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'Project2.exe'(Win32): 'C:\Windows\System32\vcruntime140d.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'Project2.exe'(Win32): 'C:\Windows\System32\ucrtbased.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+26772 스레드가 종료되었습니다(코드: 0 (0x0)).
+'Project2.exe'(Win32): 'C:\Windows\System32\kernel.appcore.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'Project2.exe'(Win32): 'C:\Windows\System32\msvcrt.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+2204 스레드가 종료되었습니다(코드: 0 (0x0)).
+16260 스레드가 종료되었습니다(코드: 0 (0x0)).
+'[24552] Project2.exe' 프로그램이 종료되었습니다(코드: 0 (0x0)).
